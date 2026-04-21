328344ab03e935e7a8a15ddeb8ce02a289f1364d
Emma Hitchens <153880994+EmEdenbrooke@users.noreply.github.com>
2026-04-20 20:22:28 -0700

    Implement showSidebar and hideSidebar functions
    Add functions to show and hide the sidebar.

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ Students must implement:
 ● functions
 ● conditional logic
 ● interactive UI behavior*/
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}

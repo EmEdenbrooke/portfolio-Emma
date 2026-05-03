36c3f102d4ba9d3de8c6acacc77cdf207de865f2
Emma Hitchens <153880994+EmEdenbrooke@users.noreply.github.com>
2026-05-02 18:33:54 -0700

    Add dark mode toggle and typing animation
    Implement dark mode toggle and typing effect for roles.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-/*JavaScript
-Students must implement:
-● DOM selection
-● event listeners
-● functions
-● conditional logic
-● interactive UI behavior*/
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded(){
+    let darkButton = document.getElementById("btn-modeSwitch")
+    darkButton.addEventListener("click", toggleDark);
+}
+function toggleDark(){
+    document.body.classList.toggle("darkmode");
+}
+
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
@@ -13,3 +16,41 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+const div = document.querySelector(".typing");
+
+
+async function startTyping(){
+    const response = await fetch('data.json');
+    const data =  await response.json();
+    const roles = data.descriptions;
+
+    let roleIndex = 0;
+    let charIndex = 0;
+    const target = document.querySelector(".typing");
+
+    function type(){
+        if (charIndex < roles[roleIndex].length) {/*if there are more characters in role add one more character*/
+            target.textContent += roles[roleIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 100);
+        } else {
+            setTimeout(erase, 2000);
+        }
+    }
+    function erase() {
+        if (charIndex > 0) {
+            // Backspacing effect (optional)
+            target.textContent = roles[roleIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, 50);
+        } else {
+            // Move to next word in the JSON array
+            roleIndex = (roleIndex + 1) % roles.length; 
+            type();
+        }
+    }
+
+    type();
+    
+}
+startTyping();
